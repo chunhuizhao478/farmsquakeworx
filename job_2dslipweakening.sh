3d35ecd4660c452db7c8065c9cf9df1a418d9c03
chunhuizhao478 <chunhui3@illinois.edu>
2024-01-30 16:40:39 -0600

    modify sh script

--- job_2dslipweakening.sh
+++ job_2dslipweakening.sh
@@ -6,7 +6,6 @@
 #SBATCH --ntasks-per-node=2
 #SBATCH --mem=2G
 #SBATCH --account=ddp408
-#SBATCH --export=ALL
-#SBATCH -t 00:10:00
+#SBATCH -t 00:01:00
 
 mpirun -n 2 /home/czhao1/farmsquakeworx/farmsquakeworx-opt -i /home/czhao1/farmsquakeworx/examples/2d_slipweakening/tpv2052D.i
